328d1ec14d6eb8eda9b91ed069995fabbc8f2acf
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2022-11-01 07:32:53 +0100

    # adding controllers and serializer

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\DTO\LowestPriceEnquiry;
+use App\Services\Serializer\DTOSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
@@ -11,9 +15,18 @@ class ProductController extends AbstractController
 
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: ['POST'])]
 
-    public function lowestPrice($id): JsonResponse
+    public function lowestPrice(Request $request, int $id, DTOSerializer $serializer): Response
     {
+        /** @var LowestPriceEnquiry $lowestPriceEnquiry */
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
 
+        $lowestPriceEnquiry->setDiscountedPrice(50);
+        $lowestPriceEnquiry->setPrice(100);
+        $lowestPriceEnquiry->setPromotionId(4);
+        $lowestPriceEnquiry->setPromotionName('Black Friday Half Price Sale');
+
+        $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json');
+        return new Response($responseContent, 200);
     }
 
 }
